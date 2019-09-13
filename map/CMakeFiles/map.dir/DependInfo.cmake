37a26361e7b10d1c2556d90130f8802808323ce5
mission_young <yuanfangsee@pku.edu.cn>
2019-09-13 17:01:45 +0800

    usual

---
+++ map/CMakeFiles/map.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/wuchenguang/work/yfs/beta_decay/map/main.cpp" "/home/wuchenguang/work/yfs/beta_decay/map/CMakeFiles/map.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/opt/ROOT/root61600/include"
+  "include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ map/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <TF1.h>
+#include <TMath.h>
+using namespace std;
+
+int main()
+{
+    cout << TMath::Pi() << endl;
+    return 0;
+}
